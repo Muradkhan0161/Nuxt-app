d2c486032ccc71c6d5a0a85f6ff941118c1223b8
Muradkhan0161 <murad.khan@northsouth.edu>
2020-03-28 00:11:08 +0600

    S3 E9 Blog post added Postlist done

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+     <section class="Post-list">
+            <PostPreview
+            id="1"
+            title="Hey there"
+            thumbnail="https://www.tececorp.com/wp-content/uploads/2019/05/050318_LRR_MEN_WomenTech-1-1.jpg"
+            post="Check out my first post"
+            />
+            <PostPreview 
+            id="2"
+            title="Hey there"
+            thumbnail="https://d30fl32nd2baj9.cloudfront.net/media/2020/01/02/technology-smart-device-010120-01.jpg/BINARY/technology-smart-device-010120-01.jpg"
+            post="Check out my second post"
+            />
+            <PostPreview 
+            id="3"
+            title="Hey there"
+            thumbnail="https://images.frandroid.com/wp-content/uploads/2020/03/coronavirus-covid-19.jpg"
+            post="Check out my third post"
+            />
+            <PostPreview 
+            id="4"
+            title="Hey there"
+            thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSddya9qHKTlBurDXgxImzz8dv29ehROkIW5YvWUd1pAJs6snVE"
+            post="Check out my forth post"
+            />
+        </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.Post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
