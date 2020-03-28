f4b0ae1a0dd5234413a8bb33dcd7620731e1d8b1
Muradkhan0161 <murad.khan@northsouth.edu>
2020-03-28 17:25:14 +0600

    S3 E11 Admin pannel and user pannel post path serparated now admin can edit post in  backend

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,24 +2,28 @@
      <section class="Post-list">
             <PostPreview
             id="1"
+            :is-admin="isAdmin"
             title="Hey there"
             thumbnail="https://www.tececorp.com/wp-content/uploads/2019/05/050318_LRR_MEN_WomenTech-1-1.jpg"
             post="Check out my first post"
             />
             <PostPreview 
             id="2"
+            :is-admin="isAdmin"
             title="Hey there"
             thumbnail="https://d30fl32nd2baj9.cloudfront.net/media/2020/01/02/technology-smart-device-010120-01.jpg/BINARY/technology-smart-device-010120-01.jpg"
             post="Check out my second post"
             />
             <PostPreview 
             id="3"
+            :is-admin="isAdmin"
             title="Hey there"
             thumbnail="https://images.frandroid.com/wp-content/uploads/2020/03/coronavirus-covid-19.jpg"
             post="Check out my third post"
             />
             <PostPreview 
             id="4"
+            :is-admin="isAdmin"
             title="Hey there"
             thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSddya9qHKTlBurDXgxImzz8dv29ehROkIW5YvWUd1pAJs6snVE"
             post="Check out my forth post"
@@ -33,7 +37,13 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
   components: {
     PostPreview
-  }
+  },
+  props: {
+    isAdmin: {
+    type: Boolean,
+    default: false
+              }
+          }
 }
 </script>
 
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm 
+            :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    components: {
+        AdminPostForm
+    },
+    data(){
+        return {
+            loadedPost: {
+                author: 'Md Murad Khan',
+                title: 'How bad is Covid-19 can be',
+                thumbnailLink: 'https://images.frandroid.com/wp-content/uploads/2020/03/coronavirus-covid-19.jpg',
+                content: 'Covid-19 recently outbreak from the wuhan city of china'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
