b0f16b5d8ce5fa87e2ffe312a0254d55bb6860a5
Muradkhan0161 <murad.khan@northsouth.edu>
2020-04-04 11:49:15 +0600

    S4 E12 adding vuex fetch and nuxtserver fetch used in same page loading

--- store/index.js
+++ store/index.js
@@ -11,6 +11,30 @@ const createStore = () => {
             }
         },
         actions: {
+            nuxtServerInit(vuexContex, context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        vuexContex.commit('setPosts', [
+                            {
+                                id: "1",
+                                title: "First Post",
+                                previewText: "This is our first post!",
+                                thumbnail:
+                                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+                              },
+                              {
+                                id: "2",
+                                title: "Second Post",
+                                previewText: "This is our second post!",
+                                thumbnail:
+                                  "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+                              }
+                        ]);
+                        resolve();
+                    }, 1000);
+                      // reject(new Error())
+                    });
+            },
             setPosts(vuexContex, posts){
                 vuexContex.commit('setPosts', posts)
             }
