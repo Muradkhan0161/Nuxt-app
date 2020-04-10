50aef71657e275e46f6e01a75644ced7825e590a
Muradkhan0161 <murad.khan@northsouth.edu>
2020-04-10 11:55:47 +0600

    S5 E7 Fetchin all the contents from firebase new-post,_id

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -43,3 +43,4 @@ export default {
   justify-content: center;
 }
 </style>
+
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -7,19 +7,23 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
-  layout: 'admin',
+  layout: "admin",
   components: {
     AdminPostForm
   },
   methods: {
     onSubmitted(postData) {
-      axios.post('https://nuxt-blog-e99f9.firebaseio.com/posts.json', postData)
+      axios
+        .post("https://nuxt-blog-e99f9.firebaseio.com/posts.json", {
+          ...postData,
+          updatedDate: new Date()
+        })
         .then(result => console.log(result))
-        .catch(e => console.log(e))
+        .catch(e => console.log(e));
     }
   }
 };
