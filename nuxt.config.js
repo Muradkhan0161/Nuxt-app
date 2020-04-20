deb0310fde2827eeb91c9d4cd580bdc365d36cc9
Muradkhan0161 <murad.khan@northsouth.edu>
2020-04-20 09:33:28 +0600

    universal app done, in meta tag scalable handleFirndly added

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     title: 'News App',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'Here will be content for the the SEO optimization' }
     ],
     link: [
